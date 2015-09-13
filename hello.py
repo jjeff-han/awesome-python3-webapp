7ffb7bda4db8449e09fc97da32ea3d31e1a3c9a8
jjeff-han <hjf11aa@126.com>
2015-09-13 19:13:38 +0800

    delete a line

--- hello.py
+++ hello.py
@@ -11,4 +11,3 @@ Git has a mutable index called stage.
 Git is free software distributed under the GPL.
 Git tracks changes of files.
 Creating a new branch is quick AND simple.
-Creating a new branch is quick AND simple.
